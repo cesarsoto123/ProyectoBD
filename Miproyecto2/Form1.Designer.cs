8016b9d0e4fb19e2a047e0f8cf1e10b0d6d41035
cesar <cesar@LAPTOP-HR14SM7D>
2024-12-02 17:44:09 -0700

    PRUEBA 2

--- Miproyecto2/Form1.Designer.cs
+++ Miproyecto2/Form1.Designer.cs
@@ -40,13 +40,14 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.inicioToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(900, 33);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -56,40 +57,40 @@
             this.clientesToolStripMenuItem,
             this.articulosToolStripMenuItem});
             this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(70, 29);
             this.inicioToolStripMenuItem.Text = "Inicio";
             // 
             // clientesToolStripMenuItem
             // 
             this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
-            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(183, 34);
             this.clientesToolStripMenuItem.Text = "Clientes";
             this.clientesToolStripMenuItem.Click += new System.EventHandler(this.clientesToolStripMenuItem_Click);
             // 
             // articulosToolStripMenuItem
             // 
             this.articulosToolStripMenuItem.Name = "articulosToolStripMenuItem";
-            this.articulosToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.articulosToolStripMenuItem.Size = new System.Drawing.Size(183, 34);
             this.articulosToolStripMenuItem.Text = "Articulos";
             // 
             // Titulo
             // 
             this.Titulo.AutoSize = true;
             this.Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Titulo.Location = new System.Drawing.Point(77, 59);
+            this.Titulo.Location = new System.Drawing.Point(87, 74);
             this.Titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Titulo.Name = "Titulo";
-            this.Titulo.Size = new System.Drawing.Size(603, 217);
+            this.Titulo.Size = new System.Drawing.Size(707, 259);
             this.Titulo.TabIndex = 2;
             this.Titulo.Text = "Programa Tiendita Para la presentacion\r\n\r\nURQUIJO CANIZALES PEDRO BRANDON S5B\r\n\r\n" +
     "JOSE PEDRO MORENO DÁVILA\r\n\r\n\r\n";
             // 
             // ClientesButton
             // 
-            this.ClientesButton.Location = new System.Drawing.Point(430, 290);
-            this.ClientesButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientesButton.Location = new System.Drawing.Point(484, 362);
+            this.ClientesButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ClientesButton.Name = "ClientesButton";
-            this.ClientesButton.Size = new System.Drawing.Size(157, 50);
+            this.ClientesButton.Size = new System.Drawing.Size(177, 62);
             this.ClientesButton.TabIndex = 3;
             this.ClientesButton.Text = "Clientes";
             this.ClientesButton.UseVisualStyleBackColor = true;
@@ -97,20 +98,20 @@
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.BackColor = System.Drawing.Color.Red;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(153, 290);
+            this.label1.Location = new System.Drawing.Point(172, 362);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 80);
+            this.label1.Size = new System.Drawing.Size(172, 100);
             this.label1.TabIndex = 4;
-            this.label1.Text = "BLUE LABEL";
+            this.label1.Text = "MARCOS CHIVO";
             // 
             // MenuPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ClientesButton);
             this.Controls.Add(this.Titulo);
